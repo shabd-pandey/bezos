6650b1763dc03010014faaee1c06b37371687349
Shabd Pandey <promotingword@gmail.com>
2026-07-21 14:37:36 +0530

    anchorize the tools and many more updates

--- script.js
+++ script.js
@@ -23,88 +23,88 @@ function updatecurrency(){
         name: "Koru superyacht",
         image: "images/koru1.jpg",
         price: 485000000,
-        type:'yatch'
+        type: "yatch"
     },
        {
         name: "Abeona",
         image: "images/abeona support yatch bezos.jpg",
         price: 485000000,
-        type: 'yatch'
+        type: "yatch"
     },
        {
         name: "GulfstreamG700",
         image: "images/Gulfstream-G700-1090x500.jpg",
         price: 80000000,
-        type:'jet' 
+        type: "jet" 
     },
        {
         name: "GulfstreamG650Er (2019)",
         image: "images/gulfsteam 2019.jpg",
         price: 65000000,
-        type:'jet' 
+        type: "jet" 
     },
     {
         name:"GulfstreamG650ER (2015)",
         image: "images/gulfsteam g650er.jpg",
         price:39000000,
-        type:'jet' 
+        type: "jet" 
             },
 
     {
         name:"Pilatus PC-24",
         image: "images/pilatus pc24.webp",
         price: 12000000,
-        type:'jet' 
+        type: "jet"
             },
     {
         name:"Dassault Falcon 900EX",
         image: "images/dassault falcon 900ex.jpg",
         price:41000000,
-        type:'jet'
+        type: "jet"
             },
     {
         name:"Koenigsegg CCXR Trevita",
         image: "images/koenigsegg ccxr trevita.avif",
         price:4800000,
-        type: 'car'
+        type: "car"
             },
     {
         name:"Lamborghini Veneno Roadster",
         image: "images/lamborghini veneno.webp",
         price: 4500000,
-        type: 'car'
+        type: "car"
             },
      {
         name:"W Motors Lykan HyperSport",
         image: "images/W Motors Lykan HyperSport.jpg",
         price: 3400000,
-        type: 'car'
+        type: "car"
             },
       {
         name:"Bugatti Veyron Mansory",
         image: "images/Bugatti Veyron Mansory.webp",
         price: 800000,
-        type: 'car'
+        type: "car"
             },
                            
         {
         name:"Ferrari Pininfarina Sergio",
         image: "images/Ferrari Pininfarina Sergio.webp",
         price: 3000000,
-        type: 'car'
+        type: "car"
             },
             
         {
         name:"Cadillac Escalade",
         image: "images/Cadillac Escalade black.jpg",
         price: 80000,
-        type: 'car'
+        type: "car"
             },
         {
         name:"Lincoln MKT Stretch Limousine",
         image: "images/Lincoln MKT stretch limousine.jpg",
         price: 150000,
-        type: 'car',
+        type: "car",
             }, 
         
       
@@ -112,25 +112,26 @@ function updatecurrency(){
         name:"Mercedes-Benz S450",
         image: "images/mercedes benz s450.jpg",
         price: 95000,
-        type: 'car'
+        type: "car"
             }, 
          {
         name:"Honda Accord (1996/1997coupe)",
         image: "images/1997 Honda Accord coupe.jpg",
         price: 150000,
-        type: 'car'
+        type: "car"
             }, 
          
                     
 
 
 ];
-function renderproduct()
+function renderproduct(filtered)
 {
-products.forEach(function(product,index){
-    document.getElementById("products").innerHTML +=`
+filtered.forEach(function(product,index){
+    document.getElementById("products").innerHTML += `
+   
 
-    <div class="card" id="cards" onclick="card_info()">
+    <div class="card"><a href="product_info.html" >
         <img src="${product.image}"class="img_product">
         <h1 class="product_name">${product.name}</h1>
         <div class="price_style">
@@ -142,10 +143,10 @@ products.forEach(function(product,index){
             <input type = "number" id="product_count_${index}" value="0" min="0" readonly class="product_count">   
             <button id="buy" onclick="buy(${index})">buy</button>
             </div>
-            </div>
+            </div></a>
     `;
 });}
- renderproduct();
+ 
 
 function sell(index)
 {
@@ -167,11 +168,17 @@ function buy(index)
 
 };
 
-function showproducts(type)
+function showProducts(type)
 {
-    
 
-}
+
+    const filtered = products.filter(product => product.type === type);
+    console.log(filtered)
+    renderproduct(filtered);
+    
+};
+
+
 
 
 
